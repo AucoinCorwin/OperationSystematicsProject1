3f72edd585d535a7f2ab5efd666cd46b238a4d48
Darien Keyack <keyacd@rpi.edu>
2017-03-09 23:56:23 +0000

    Just in case I missed anything

--- printing.c
+++ printing.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "msg.h"
 
 // Darien Keyack (661190088) and Corwin Aucoin (661178786)
@@ -16,11 +17,22 @@ int main(int argc, char *argv[]) {
     // Go through the outputs
     char line_g[100];
     char line_t[100];
+    int i;
+    int len;
     while (fgets(line_g, sizeof(line_g), gen) != NULL) {
         fgets(line_t, sizeof(line_t), out);
-        if (line_g != line_t) {
-            printf("Invalid match! %s and %s\n", line_g, line_t); 
+        if (line_g[10] == 'S') {
+            printf("\n%s**********************************************\n", line_g);
             fflush(stdout);
+        }
+        len = strlen(line_g);
+        if (len > strlen(line_t)) len = strlen(line_t);
+        for (i = 0; i < len; i++) {
+            if (line_g[i] != line_t[i]) {
+                printf("%c != %c; invalid match! %s and %s", line_g[i], line_t[i], line_g, line_t);
+                fflush(stdout);
+                break;
+            }
         }
     }
     
