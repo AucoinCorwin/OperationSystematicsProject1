75f3865be78f3fb99af2539a14531807b5bf9de2
Darien Keyack <keyacd@rpi.edu>
2017-03-09 19:15:40 +0000

    fixed input01 debugging

--- out.h
+++ out.h
@@ -10,44 +10,40 @@ void debug_float(float f, float v, FILE *output) {
 }
 
 void debug_int(int i, int v, FILE *output) {
-    if (i != v) fprintf(output, "** ERROR: correct value %i\n", i);
+    if (i != v) fprintf(output, "** ERROR: correct value %i **\n", i);
 }
 
-void out_params(char* algo, char* input, FILE *output, float burst, int wait_total, int wait_count, int turnaround_total, int turnaround_count, int switches, int preempts) {
+void out_params(char* input, char* algo, FILE *output, float burst, int wait_total, int wait_count, int turnaround_total, int turnaround_count, int switches, int preempts) {
     fprintf(output, "Algorithm %s\n", algo);
     fprintf(output, "-- average CPU burst time: %.2f ms\n", burst);
     #ifdef DEBUG
-        float f = 403.85;
-        debug_float(f, burst, output);
+        debug_float(403.85, burst, output);
     #endif
     float wait = avg(wait_total, wait_count);
     fprintf(output, "-- average wait time: %.2f ms\n", wait);
     #ifdef DEBUG
-        if (algo == "FCFS") f = 356.46;
-        else if (algo == "SRT") f = 140.38;
-        else f = 284.92;
-        debug_float(f, wait, output);
+        if (algo == "FCFS") debug_float(356.46, wait, output);
+        else if (algo == "SRT") debug_float(140.38, wait, output);
+        else debug_float(284.92, wait, output);
     #endif
     float turnaround = avg(turnaround_total, turnaround_count);
     fprintf(output, "-- average turnaround time: %.2f ms\n", turnaround);
     #ifdef DEBUG
-        if (algo == "FCFS") f = 766.31;
-        if (algo == "SRT") f = 551.85;
-        else f = 700.08;
-        debug_float(f, turnaround, output);
+        if (algo == "FCFS") debug_float(766.31, turnaround, output);
+        else if (algo == "SRT") debug_float(551.85, turnaround, output);
+        else debug_float(700.08, turnaround, output);
     #endif
     fprintf(output, "-- total number of context switches: %i\n", switches);
     #ifdef DEBUG
         int i;
-        if (algo == "FCFS") i = 13;
-        if (algo == "SRT") i = 20;
-        else i = 36;
-        debug_int(i, switches, output);
+        if (algo == "FCFS") debug_int(13, switches, output);
+        else if (algo == "SRT") debug_int(20, switches, output);
+        else debug_int(36, switches, output);
     #endif
     fprintf(output, "-- total number of preemptions: %i\n", preempts);
     #ifdef DEBUG
-        if (algo == "FCFS") i = 0;
-        if (algo == "SRT") i = 7;
-        else i = 23;
+        if (algo == "FCFS") debug_int(0, preempts, output);
+        else if (algo == "SRT") debug_int(7, preempts, output);
+        else debug_int(23, preempts, output);
     #endif
 }
